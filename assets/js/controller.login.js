9a03b8cae9379ed45eb9d84b3db4e20f0034dade
Nicolas <nicojs2011@gmail.com>
2024-11-14 15:20:38 -0400

    controlador de login agregado

--- assets/js/controller.login.js
+++ assets/js/controller.login.js
@@ -1,5 +1,28 @@
 $("#submit").click(function (e) { 
     e.preventDefault();
-    console.log("Hola");
-    
+
+    const user = $("#user").val()
+    const pass = $("#pass").val()
+    const check = document.getElementById("check").checked
+
+    console.log({user,pass,check})
+
+    const verification = async (user,pass) => {
+        try {
+            const response = await fetch('http://localhost:3000/login/', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: {user,pass},
+            });
+
+            const res = await response.json();
+            // const data = res.data
+            console.log(res);
+            
+        }catch (error) {
+            console.error('Error:', error);
+        }
+    }
 });
